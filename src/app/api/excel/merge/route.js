8c68c13612e083f7dc8b43939388cbaaeeb8f6dd
Jumair AJ <83821197+Jumair-aj@users.noreply.github.com>
2024-11-01 14:18:56 +0530

    Update route.js

--- src/app/api/excel/merge/route.js
+++ src/app/api/excel/merge/route.js
@@ -6,7 +6,7 @@ import ExcelJS from 'exceljs';
 import { writeFile } from 'fs/promises';
 import { v4 as uuidv4 } from 'uuid';
 
-export const maxDuration = 60;
+
 const excelToJSON = (buffer, sheetIndex) => {
   const workbook = XLSX.read(buffer);
   const sheetName = workbook.SheetNames[sheetIndex];
@@ -472,3 +472,4 @@ export async function OPTIONS() {
 }
 
 
+export const maxDuration = 60;
